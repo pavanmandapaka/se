2d4aa79b4002b532cd79f5ae75d7448796304991
kpsr01 <pranavsuhasreddykonduru@gmail.com>
2026-02-03 08:54:11 +0530

    Add/Update pws1.cpp (knapsack implementation)

--- pws1.cpp
+++ pws1.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 
 
-int knapsack(int W, vector<int>& weights, vector<int>& values, int n) {
+int knapsack(int W, vector<int>& weights, vector<int>& v, int n) {
     vector<vector<int>> dp(n + 1, vector<int>(W + 1, 0));
     
     for (int i = 1; i <= n; i++) {
         for (int w = 1; w <= W; w++) {
             if (weights[i-1] <= w) {
-                dp[i][w] = max(values[i-1] + dp[i-1][w - weights[i-1]], 
+                dp[i][w] = max(v[i-1] + dp[i-1][w - weights[i-1]], 
                                dp[i-1][w]);
             } else {
                 dp[i][w] = dp[i-1][w];
@@ -22,13 +22,13 @@ int knapsack(int W, vector<int>& weights, vector<int>& values, int n) {
 }
 
 // Space-optimized version using 1D array
-int knapsackOptimized(int W, vector<int>& weights, vector<int>& values, int n) {
+int knapsackOptimized(int W, vector<int>& weights, vector<int>& v, int n) {
     vector<int> dp(W + 1, 0);
     
     for (int i = 0; i < n; i++) {
         // Traverse from right to left to avoid overwriting
         for (int w = W; w >= weights[i]; w--) {
-            dp[w] = max(dp[w], values[i] + dp[w - weights[i]]);
+            dp[w] = max(dp[w], v[i] + dp[w - weights[i]]);
         }
     }
     
@@ -44,11 +44,11 @@ int main() {
     cout << "Enter knapsack capacity: ";
     cin >> W;
     
-    vector<int> values(n), weights(n);
+    vector<int> v(n), weights(n);
     
     cout << "Enter values of items:\n";
     for (int i = 0; i < n; i++) {
-        cin >> values[i];
+        cin >> v[i];
     }
     
     cout << "Enter weights of items:\n";
@@ -56,10 +56,10 @@ int main() {
         cin >> weights[i];
     }
     
-    int maxValue = knapsack(W, weights, values, n);
+    int maxValue = knapsack(W, weights, v, n);
     cout << "\nMaximum value (2D DP): " << maxValue << endl;
     
-    int maxValueOptimized = knapsackOptimized(W, weights, values, n);
+    int maxValueOptimized = knapsackOptimized(W, weights, v, n);
     cout << "Maximum value (1D DP): " << maxValueOptimized << endl;
     
     return 0;
